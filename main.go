5b5de6cee47ac7e646714ab4221e3ec594e8af5c
Jeff Wu <itmrchow@gmail.com>
2025-03-18 15:46:46 +0800

    feat: add "initConfig"

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	initConfig()
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal().Err(err).Msg("config init error")
+	}
+
+	log.Info().Msgf("config init success")
+}
