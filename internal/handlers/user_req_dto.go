ad9e12d12f2e34c63d8bdedbd218cd25bd7f90d7
Jeff Wu <itmrchow@gmail.com>
2025-03-20 17:30:06 +0800

    feat: add "validate"

--- internal/handlers/user_req_dto.go
+++ internal/handlers/user_req_dto.go
@@ -1,12 +1,12 @@
 package handlers
 
 type RegisterUserReqDTO struct {
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+	Email    string `validate:"required,email" json:"email"`
+	Name     string `validate:"required,min=1,max=20" json:"name"`
+	Password string `validate:"required,min=6,max=20" json:"password"`
 }
 
 type LoginUserReqDTO struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `validate:"required,email" json:"email"`
+	Password string `validate:"required,min=6,max=20" json:"password"`
 }
